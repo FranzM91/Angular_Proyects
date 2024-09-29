5ded7c0e85f891217145cab34c202bd0496fb313
Franz Mamani <franzmamanich1991@gmail.com>
2024-09-28 23:31:18 -0400

    update repository

--- TodoApp/main/src/app/app.module.ts
+++ TodoApp/main/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // import { AppRoutingModule } from './app-routing.module';
 import { StyleClassModule } from 'primeng/styleclass';
 import { AppComponent } from './app.component';
+import { MenuModule } from 'primeng/menu';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
     imports: [
@@ -17,6 +19,8 @@ import { AppComponent } from './app.component';
         BrowserAnimationsModule,
         StyleClassModule,
         // import Module from PrimeNG
+        MenuModule,
+        InputTextModule,
     ],
     declarations: [
         AppComponent,
