c9d7f1e7bba792a72defabb80f735dda7a23e199
FM <franz.mamani@qhanati.com>
2024-10-09 12:19:22 -0400

    update proyect

--- TodoApp/master/src/app/demo/components/formulario/formulariocomponent/formulario.component.ts
+++ TodoApp/master/src/app/demo/components/formulario/formulariocomponent/formulario.component.ts
@@ -1,15 +1,29 @@
 import { Component } from "@angular/core";
+import { Usuario } from "../shared/usuario.model";
 
 @Component({
     templateUrl: './formulario.component.html'
 })
 export class FormularioComponent {
-    nombre = "";
-    apellido = "";
-    fechaNacimiento = Date();
-
+    // nombre = "";
+    // apellido = "";
+    // fechaNacimiento = Date();
+    selectEntity: Usuario;
+    dataSource: Usuario[] = [];
     constructor() {
+        this.reset();
     }
-
-    btnSave() {}
+    reset() {
+        this.selectEntity = new Usuario();
+    }
+    btnSave() {
+        // var data = "Nombre:" + this.nombre + "||  Apellido:" + this.apellido;
+        // alert(`Nombre: ${this.selectEntity.nombre} || Apellido: ${this.selectEntity.apellido} || Fecha: ${this.selectEntity.fechaNacimiento}`);
+        this.dataSource.push(this.selectEntity);
+        console.log(this.dataSource);
+        this.reset();
+    }
+    btnCancel() {
+        this.reset();
+    }
 }
